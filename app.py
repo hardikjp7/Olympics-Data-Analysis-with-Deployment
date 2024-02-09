d2fa35b75732bac4176e24c986b5532f19c90d5c
Hardik Parmar <hardikjparmar7@gmail.com>
2024-02-09 14:02:36 +0530

    update

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.figure_factory as ff
 
-df = pd.read_csv('data/athlete_events.csv')
-region_df = pd.read_csv('data/noc_regions.csv')
+df = pd.read_csv('athlete_events.csv')
+region_df = pd.read_csv('noc_regions.csv')
 
 df = preprocessor.preprocess(df,region_df)
 
